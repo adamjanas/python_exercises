d777fcc60012bd2e017bc704ac84cbf434f28c21
Adam <a.janas@protonmail.com>
2019-04-03 23:59:17 +0200

    tasks

--- python course/datetime.py
+++ python course/datetime.py
@@ -2,4 +2,4 @@ import datetime
 
 teraz = datetime.datetime.now()
 print(str(teraz.hour)+":"+str(teraz.minute))
-print(teraz.strftime("%H:%M   %d.%m.%Y"))     # alternatywa     print(teraz.strftime("%H:%M  %d %b %Y) Nazwa miesiaca
+print(teraz.strftime("%H:%M   %d.%m.%Y"))     # alternative     print(teraz.strftime("%H:%M  %d %b %Y) Nazwa miesiaca
