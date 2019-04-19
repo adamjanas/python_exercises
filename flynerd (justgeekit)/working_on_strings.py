e79bbfdcac5122eaf93b0306c780a34d1118ebc9
Adam <a.janas@protonmail.com>
2019-04-19 14:04:43 +0200

    tasks

--- flynerd (justgeekit)/working_on_strings.py
+++ flynerd (justgeekit)/working_on_strings.py
@@ -26,3 +26,5 @@ print(r"without functions like \n \t")
 print(a.split(","))    #it divides string in base of (,), split() divides every word in string
 print(sorted([1, 6, 2, 9, 3, 5, 2, 7], reverse = True))
 print(round(12.3141592326, 2)) #it rounds places after point
+print(format(b, '.50f'))        #it displays amount of numbers after point
+print("%.50f" % b)              #the same (alternative)
