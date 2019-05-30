3a85af772b17d8ff9f9172cab32ff9eb2a18fcea
Adam <a.janas@protonmail.com>
2019-05-30 20:24:35 +0200

    tasks

---
+++ python course/object_oriented_programming4.py
@@ -0,0 +1,24 @@
+class human:
+    name = "unknown"
+    __age = 00
+
+    def __init__(self, name, age):
+        self.name = name
+        self._age = age
+
+    def set_age(self, age):
+        self.__age = age
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_age(self):
+        if self.__age > 18:
+            print("adult")
+        elif self.__age > 10:
+            print("teenager")
+        else:
+            print("child")
+
+man = human("john", 24)
+man.get_age()
