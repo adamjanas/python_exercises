85371811c891138208332e13f006dcb42fa4fb33
Adam <a.janas@protonmail.com>
2019-05-31 15:11:14 +0200

    tasks

--- python course/class_inheritance1.py
+++ python course/class_inheritance1.py
@@ -11,11 +11,25 @@ class Mammal(Animal):
     def introduce_yourself(self):
         print("introduce yourself")
 
+class Cat(Mammal):
+    def __init__(self):
+        Mammal.__init__(self)
+        print("cat")
+    def purr(self):
+        print("purr")
+
+
+
+
+
+
 mammal_1 = Mammal()
 mammal_1.introduce_yourself()
 mammal_1.increase_age()
 
 
-import os
+cat_1 = Cat()
+cat_1.purr()
+cat_1.increase_age()
+cat_1.introduce_yourself()
 
-os.rename("test.py", "class_inheritance1.py")
