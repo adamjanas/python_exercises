016b057e0a3aaa961015bf86b319ef4c9ba1d286
Adam <a.janas@protonmail.com>
2019-06-19 23:52:03 +0200

    LPTHW tasks

--- learn_python_the_hard_way/ex25.py
+++ learn_python_the_hard_way/ex25.py
@@ -7,12 +7,13 @@ def sort_words(words):
     """Sort the words"""
     return sorted(words)
 def print_first_word(words):
-    """Print first word"""
-    word = words.pop[0]
+    """Print the first word after popping it off"""
+    word = words.pop(0)
     print(word)
 def print_last_word(words):
-    """Print last word"""
-    word = words.pop[-1]
+    """Print the last word after popping it off"""
+    word = words.pop(-1)
+    print(word)
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words"""
     words = break_words(sentence)
@@ -23,8 +24,8 @@ def print_first_and_last(sentence):
     print_first_word(words)
     print_last_word(words)
 def print_first_and_last_sorted(sentence):
-    words = sort_sentence()
-    print_last_word(words)
+    words = sort_sentence(sentence)
+    print_first_word(words)
     print_last_word(words)
 
 
