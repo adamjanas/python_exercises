a4901922d1bdeeaeea9b0eda847ffb7b7c995cb3
Adam <a.janas@protonmail.com>
2019-07-01 21:58:33 +0200

    LPTHW tasks

--- learn_python_the_hard_way/ex39.py
+++ learn_python_the_hard_way/ex39.py
@@ -16,6 +16,39 @@ cities = {
 cities['NY'] = 'New York'
 cities['OR'] = 'Portland'
 
-print('-'*10)
+print('-' * 10)
 print("NY State has:", cities['NY'])
 print("OR State has:", cities['OR'])
+
+
+print('-' * 10)
+print("Michigan's abreaviation is:", states['Michigan'])
+print("Florida's abbreaviation is:", states['Florida'])
+
+print('-' * 10)
+print('Michigan has:', cities[states['Michigan']])
+print('Florida has:', cities[states['Florida']])
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
+
+print('-' * 10)
+for abbrev, city in list(cities.items()):
+    print(f"{abbrev} has the city {city}")
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas.")
+
+city = cities.get('TX', 'Does not exist')
+print(f"The city for the state 'TX' is: {city}")
+
+#print(city.get('if TX exists display tx's key', 'if not display this')
